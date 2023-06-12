4ae962123d67f154c6a6bda157517036323cc827
JoDaeUk <du39574@gmail.com>
2023-06-12 10:39:06 +0900

    새싹

---
+++ src/새싹.java
@@ -0,0 +1,14 @@
+
+public class 새싹 {
+    public static void main(String[] args) {
+        /*Todo 아래 예제와 같이 새싹을 출력하시오.*/
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+
+
+    }
+}
