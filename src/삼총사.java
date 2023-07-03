90881506b54a6c3e29a1e2cf942642c86224fbbe
JoDaeUk <du39574@gmail.com>
2023-07-03 17:04:09 +0900

    삼총사

---
+++ src/삼총사.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class 삼총사 {
+    public static void main(String[] args) {
+        int[] number1 ={-2, 3, 0, 2, -5};
+        int[] number2 ={-3, -2, -1, 0, 1, 2, 3};
+        int[] number3 ={-1, 1, -1, 1};
+
+        System.out.println(solution(number1));
+        System.out.println(solution(number2));
+        System.out.println(solution(number3));
+    }
+    static public int solution(int[] number) {
+        int answer = 0;
+        for (int i=0; i<number.length; i++){
+            for (int j=i+1; j<number.length; j++){
+                for (int k=j+1; k<number.length; k++){
+                    if(number[i]+number[j]+number[k]==0) answer++;
+
+                }
+            }
+        }
+        return answer;
+    }
+}
