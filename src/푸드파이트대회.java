c5ee15c460aa0a232ffd981cdcf3558cf2074b46
JoDaeUk <du39574@gmail.com>
2023-07-06 16:40:45 +0900

    푸드파이트대회

---
+++ src/푸드파이트대회.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class 푸드파이트대회 {
+    public static void main(String[] args) {
+        int[] food1 = {1,3,4,6};
+        int[] food2 = {1,7,1,2};
+
+        System.out.println(solution(food1));
+        System.out.println(solution(food2));
+
+    }
+
+    static public String solution(int[] food) {
+        String answer = "";
+        int count = 0;
+        for (int i=0; i<food.length; i++){
+            while (food[i]/2>count){
+                answer += i;
+                count++;
+            }
+            count=0;
+        }
+        int len = answer.length();
+        answer += 0;
+        for (int i=answer.length()-2; i>=0; i--){
+            answer += answer.charAt(i);
+        }
+        return answer;
+    }
+}
