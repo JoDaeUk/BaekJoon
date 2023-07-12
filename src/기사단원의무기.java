9f8f15ee7c8e88c5d322c327984a93b6344dab83
JoDaeUk <du39574@gmail.com>
2023-07-12 17:23:41 +0900

    기사단원의무기

---
+++ src/기사단원의무기.java
@@ -0,0 +1,20 @@
+
+public class 기사단원의무기 {
+    public static void main(String[] args) {
+        System.out.println(solution(5,3,2));
+        System.out.println(solution(10,3,2));
+    }
+
+    static public int solution(int number, int limit, int power) {
+        int answer = 0;
+        for (int i =1; i<=number; i++){
+            int count = 0;
+            for (int j=1; j*j<=i; j++){
+                if (j * j == i) count++;
+                else if (i % j == 0) count += 2;
+            }
+            answer += count > limit ? power : count;
+        }
+        return answer;
+    }
+}
