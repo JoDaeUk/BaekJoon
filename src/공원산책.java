278f59fb9bf9e481ccfc039f6a1cf30301a88f24
JoDaeUk <du39574@gmail.com>
2023-09-11 18:45:03 +0900

    공원산책

--- src/공원산책.java
+++ src/공원산책.java
@@ -28,27 +28,54 @@ public class 공원산책 {
 
     }
     static public int[] solution(String[] park, String[] routes) {
-        int numRows = park.length;
-        int numCols = park[0].length();
-        int[] location = {0, 0};
-        String[][] parkArray = new String[numRows][numCols];
+        int sx = 0;
+        int sy = 0;
 
-        for (int i = 0; i < numRows; i++) {
-            parkArray[i] = park[i].split("");
+        char[][] arr = new char[park.length][park[0].length()];
+
+        for(int i = 0; i < park.length; i++){
+            arr[i] = park[i].toCharArray();
+
+            if(park[i].contains("S")){
+                sy = i;
+                sx = park[i].indexOf("S");
+            }
         }
-        for (int i=0; i<parkArray.length; i++){
-            for (int j=0; j<parkArray[i].length; j++){
-                if(parkArray[i][j].equals("S")) {
-                    location = new int[]{i, j};
-                    break;
+
+        for(String st : routes){
+            String way = st.split(" ")[0];
+            int len = Integer.parseInt(st.split(" ")[1]);
+
+            int nx = sx;
+            int ny = sy;
+
+            for(int i = 0; i < len; i++){
+                if(way.equals("E")){
+                    nx++;
+                }
+                if(way.equals("W")){
+                    nx--;
+                }
+                if(way.equals("S")){
+                    ny++;
+                }
+                if(way.equals("N")){
+                    ny--;
+                }
+                if(nx >=0 && ny >=0 && ny < arr.length && nx < arr[0].length){
+                    if(arr[ny][nx] == 'X'){
+                        break;
+                    }
+                    // 범위내 & 장애물 x
+                    if(i == len-1){
+                        sx = nx;
+                        sy = ny;
+                    }
                 }
             }
         }
 
-        for (String route : routes){
-            String[] sRoute = route.split("");
-
-            }
-        return null;
-        }
+        int[] answer = {sy, sx};
+        return answer;
     }
+}
