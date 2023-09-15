feb7a0a2890fd881fbc2115f629a2920bdfd1d89
JoDaeUk <du39574@gmail.com>
2023-09-15 17:36:47 +0900

    성격유형검사하기1

---
+++ src/성격유형검사하기.java
@@ -0,0 +1,24 @@
+
+
+public class 성격유형검사하기 {
+    public static void main(String[] args) {
+
+        String[] survey1 = {"AN", "CF", "MJ", "RT", "NA"};
+        String[] survey2 = {"TR", "RT", "TR"};
+
+        int[] choices1 = {5, 3, 2, 7, 5};
+        int[] choices2 = {7, 1, 3};
+
+        System.out.println(solution(survey1,choices1));
+        System.out.println(solution(survey2,choices2));
+
+    }
+    static public String solution(String[] survey, int[] choices) {
+        int r=0,t=0,c=0,f=0,j=0,m=0,a=0,n=0;
+        for (int i=0; i<survey.length; i++){
+            if (choices[i]==1)
+
+        }
+    }
+}
+
