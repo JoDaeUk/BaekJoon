b9499a76ab20db28e996d70647faf279f68dac83
JoDaeUk <du39574@gmail.com>
2023-10-11 19:26:42 +0900

    행열의 덧샘

---
+++ src/행열의덧샘.java
@@ -0,0 +1,9 @@
+public class 행열의덧샘 {
+    public static void main(String[] args) {
+    }
+    static public String solution(int num) {
+        if (num % 2 == 0) return ("Even");
+        else return ("Odd");
+
+    }
+}
