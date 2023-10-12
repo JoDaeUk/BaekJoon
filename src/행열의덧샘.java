c89ffc80b8e95524ac72e2fe2411c982ee89128d
JoDaeUk <du39574@gmail.com>
2023-10-12 18:52:42 +0900

    행열의 덧샘1

--- src/행열의덧샘.java
+++ src/행열의덧샘.java
@@ -1,9 +1,22 @@
+import java.util.Arrays;
+
 public class 행열의덧샘 {
     public static void main(String[] args) {
-    }
-    static public String solution(int num) {
-        if (num % 2 == 0) return ("Even");
-        else return ("Odd");
+        int[][] arr11 ={{1,2},{2,3}};
+        int[][] arr12 ={{1},{2}};
 
+        int[][] arr21 ={{3,4},{5,6}};
+        int[][] arr22 ={{3},{4}};
+
+        System.out.println(Arrays.deepToString(solution(arr11, arr21)));
+        System.out.println(Arrays.deepToString(solution(arr12, arr22)));
+    }
+    static int[][] solution(int[][] arr1, int[][] arr2) {
+        for(int i=0 ; i<arr1.length ; i++) {
+            for(int j=0 ; j<arr1[0].length ; j++) {
+                arr1[i][j] += arr2[i][j];
+            }
+        }
+        return arr1;
     }
 }
