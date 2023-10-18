5bc4baf5a476ca5e0d0c6d14d6c4b17ff3426166
JoDaeUk <du39574@gmail.com>
2023-10-18 18:05:12 +0900

    전화번호가리기(예시 추가)

--- src/전화번호가리기.java
+++ src/전화번호가리기.java
@@ -1,6 +1,8 @@
 public class 전화번호가리기 {
     public static void main(String[] args) {
         System.out.println(solution("01033334444"));
+        System.out.println(solution("027778888"));
+
 
     }
 
