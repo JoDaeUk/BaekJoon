77f25edc5123e65dc295ad96d63b8921d6582ce6
JoDaeUk <du39574@gmail.com>
2023-10-19 18:18:49 +0900

    문자열 숫자로바꾸기

---
+++ src/문자열정수로바꾸기.java
@@ -0,0 +1,12 @@
+public class 문자열정수로바꾸기 {
+    public static void main(String[] args) {
+        System.out.println(solution("-1234"));
+
+
+    }
+
+    static public int solution(String s) {
+        int intValue = Integer.parseInt(s);
+        return intValue;
+    }
+    }
